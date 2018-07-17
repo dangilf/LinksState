7b2f88104f78ce9d39f08d2ba1d93ec6202222d4
Danil <danilgilfanov@gmail.com>
2018-07-17 19:04:07 +0300

    Initial

---
+++ LinksStateReportViewer/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace LinksStateReportViewer.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ LinksStateReportViewer/Areas/HelpPage/ModelDescriptions/EnumValueDescription.cs
@@ -0,0 +1,11 @@
+namespace LinksStateReportViewer.Areas.HelpPage.ModelDescriptions
+{
+    public class EnumValueDescription
+    {
+        public string Documentation { get; set; }
+
+        public string Name { get; set; }
+
+        public string Value { get; set; }
+    }
+}
